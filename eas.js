7f6bcece6b83d828ecf148925d3985a38a9342b4
Ryan <rygho90@gmail.com>
2022-05-01 19:25:09 -0400

    Add ability to change grid size

--- eas.js
+++ eas.js
@@ -1,6 +1,27 @@
 const easContainer = document.querySelector('.eas-container');
+const sizeButton = document.querySelector('.btn');
+let gridSize = 16;
 
-for (let i = 0; i < (16*16); i++) {
-    let newDiv = document.createElement('div');
-    easContainer.append(newDiv);
+function drawGrid() {
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        let gridBox = document.createElement('div');
+        gridBox.addEventListener('mouseover', () => {
+            gridBox.style.backgroundColor = 'black';
+        })
+        easContainer.append(gridBox);
+    }
 }
+
+drawGrid();
+
+sizeButton.addEventListener('click', () => {
+    gridSize = prompt("Enter a grid size (1-100): ")
+    while (easContainer.firstChild) {
+        easContainer.removeChild(easContainer.lastChild);
+    }
+    if (gridSize <= 100 && gridSize >= 1) {
+        easContainer.style['grid-template-columns'] = `repeat(${gridSize}, 1fr)`;
+        easContainer.style['grid-template-rows'] = `repeat(${gridSize}, 1fr)`;
+        drawGrid();
+    }
+})
